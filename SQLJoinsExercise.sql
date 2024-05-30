f3313547b45425821c4cb9431b084579139c1233
Sam Shadwell <sshadwell978@gmail.com>
2024-05-30 15:38:12 -0400

    Completed all exercises

--- SQLJoinsExercise.sql
+++ SQLJoinsExercise.sql
@@ -1,16 +1,49 @@
 /* joins: select all the computers from the products table:
 using the products table and the categories table, return the product name and the category name */
- 
+SELECT p.Name AS "Product Name", c.Name AS "Category Name"
+FROM products AS p
+INNER JOIN categories AS c
+ON p.CategoryID = c.CategoryID
+WHERE p.CategoryID = 1;
+
 /* joins: find all product names, product prices, and products ratings that have a rating of 5 */
+ SELECT p.Name, p.Price, r.Rating
+ FROM products AS p
+ INNER JOIN reviews AS r
+ ON p.ProductID = r.ProductID
+ WHERE Rating = 5;
  
 /* joins: find the employee with the most total quantity sold.  use the sum() function and group by */
+SELECT e.EmployeeID, e.FirstName, e.LastName, SUM(Quantity) AS Quantity
+FROM employees AS e
+INNER JOIN sales AS s
+ON e.EmployeeID = s.EmployeeID
+GROUP BY e.EmployeeID
+ORDER BY Quantity DESC;
 
 /* joins: find the name of the department, and the name of the category for Appliances and Games */
+SELECT c.Name AS "Category Name", d.Name AS "Department Name"
+FROM categories AS c
+INNER JOIN departments AS d
+ON c.DepartmentID = d.DepartmentID
+WHERE c.Name = 'Games' OR c.Name = 'Appliances';
 
 /* joins: find the product name, total # sold, and total price sold,
  for Eagles: Hotel California --You may need to use SUM() */
+SELECT p.Name, SUM(s.Quantity) AS Quantity, SUM(s.PricePerUnit) AS Price
+FROM products AS p
+INNER JOIN sales AS s
+ON p.ProductID = s.ProductID
+WHERE p.Name LIKE "%eagles: hotel%"
+GROUP BY p.Name;
 
 /* joins: find Product name, reviewer name, rating, and comment on the Visio TV. (only return for the lowest rating!) */
+SELECT p.Name, r.Reviewer, r.Rating, r.Comment
+FROM products AS p
+INNER JOIN reviews AS r
+ON p.ProductID = r.ProductID
+WHERE p.Name = "Visio TV" AND r.Rating = 1;
+
 
 
 -- ------------------------------------------ Extra - May be difficult
@@ -20,3 +53,13 @@ This query should return:
 -  the employee's first and last name
 -  the name of each product
 -  and how many of that product they sold */
+
+SELECT e.EmployeeID, e.FirstName, e.LastName, p.Name, s.Quantity
+FROM sales AS s
+INNER JOIN products AS p
+ON s.ProductID = p.ProductID
+INNER JOIN employees AS e
+ON s.EmployeeID = e.EmployeeID
+ORDER BY e.EmployeeID;
+
+
